92f297e533c6c2e0eb2fd01bd66afe9d9cc5c60e
Andy Ng <97724421+andyng-sv@users.noreply.github.com>
2022-10-26 17:33:56 +0800

    Edited for 2 channels

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+date=$(date +"%d-%m-%H-%M")
 
-ffmpeg -i rtsp://root:Vaxtor00@10.0.0.54:554/live1s2.sdp -strict -2 -b:v 900K -vcodec copy -r 8 -t 02:05:00 -y /home/administrator/Desktop/test.mp4
+#directory path of recording
+_path="/home/administrator/Desktop/$_fullpath"
+#recording path
+_rpath="/home/administrator/Recordings/$date/"
+mkdir -p /home/administrator/Recordings/$date
 
+ffmpeg -i rtsp://root:Vaxtor00@192.168.123.2:554/live1s2.sdp -strict -2 -b:v 100K -vcodec copy -f segment -segment_time 60 -reset_timestamps 1 -strftime 1 -t 02:05:00 -y $_rpath%H%M.mp4

1f9a3e74ad37ae77745d58224f40468f1181e928
Andy Ng <97724421+andyng-sv@users.noreply.github.com>
2022-11-08 12:03:14 +0800

    Updated bitrate

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -8,4 +8,4 @@ _path="/home/administrator/Desktop/$_fullpath"
 _rpath="/home/administrator/Recordings/$date/"
 mkdir -p /home/administrator/Recordings/$date
 
-ffmpeg -i rtsp://root:Vivotek00@192.168.123.1:554/live1s1.sdp -strict -2 -b:v 300K -vcodec copy -f segment -segment_time 60 -reset_timestamps 1 -strftime 1 -t 02:05:00 -y $_rpath%H%M.mp4
+ffmpeg -i rtsp://root:Vivotek00@192.168.123.1:554/live1s1.sdp -strict -2 -b:v 500k -vcodec copy -f segment -segment_time 60 -reset_timestamps 1 -strftime 1 -t 02:05:00 -y $_rpath%H%M.mp4
